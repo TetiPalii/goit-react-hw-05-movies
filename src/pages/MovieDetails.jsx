8d0d9728573817c5f6f3e76075a88bb0484b4aad
TetiPalii <TetianaPalii9491@gmail.com>
2023-07-08 17:04:39 +0200

    commit

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -2,7 +2,7 @@ import { Outlet, useParams } from 'react-router-dom';
 
 const MovieDetails = () => {
   const id = useParams();
-  return <div></div>;
+  return <div>{id}</div>;
 };
 
 export default MovieDetails;
